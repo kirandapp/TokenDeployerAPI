5d82166092054eac77083c82e988b260ca88c1d2
kiran <kiranjha.spearmint@gmail.com>
2023-06-22 17:01:02 +0530

    vercel changes

--- api/app.js
+++ api/app.js
@@ -101,7 +101,7 @@ const fs = require('fs');
 const Web3 = require('web3').Web3;
 const solc = require('solc');
 const path = require('path');
-
+const port = process.env.PORT || 3000;
 const app = express();
 
 app.get('/', (req, res) => {
@@ -112,93 +112,118 @@ app.use(express.json());
 
 const contractCache = {}; // Object to store contract source code
 const artifactsCache = {}; // Object to store contract artifacts
+const contractArtifacts = {};
+// app.post('/api/deploy-bigeyes', async (req, res) => {
 
 app.post('/api/deploy-bigeyes', async (req, res) => {
   const { contractName, templateName, name, symbol, decimals, totalSupply, privateKey } = req.body;
 
-  // Read the template contract
-  const contractTemplate = fs.readFileSync(path.join(__dirname, '..', 'Templates', `${templateName}.sol`), 'utf8');
-
-  // Replace the placeholder with the user-provided name in the contract template
-  const updatedContractSource = contractTemplate.replace('CONTRACTNAME', contractName);
-
-  // Check if the replacement occurred successfully
-  if (updatedContractSource === contractTemplate) {
-    return res.status(400).json({ error: "Placeholder 'CONTRACTNAME' not found in the contract template." });
-  }
-
-  // // Write the updated contract to a temporary file
-  // fs.writeFileSync(path.join(__dirname, '..', 'Generated', `${contractName}.sol`), updatedContractSource, 'utf8');
-
-  contractCache[contractName] = updatedContractSource;
-
-  // // Compile the updated contract
-  // const contractSourceCode = fs.readFileSync(path.join(__dirname, '..', 'Generated', `${contractName}.sol`), 'utf8');
-  const contractSourceCode = contractCache[contractName];
-  const input = {
-    language: 'Solidity',
-    sources: {
-      [`${contractName}.sol`]: {
-        content: contractSourceCode,
-      },
-    },
-    settings: {
-      outputSelection: {
-        '*': {
-          '*': ['*'],
-        },
-      },
-      optimizer: {
-        enabled: true,
-        runs: 200, // Set the number of optimization runs here
-      },
-    },
-  };
-
-  const output = JSON.parse(solc.compile(JSON.stringify(input)));
-
-  // Check for compilation errors
-  if (output.errors && output.errors.length > 0) {
-    const errors = output.errors.filter((error) => String(error).includes('Error'));
-    if (errors.length > 0) {
-      console.error('Contract compilation failed:');
-      errors.forEach((error) => {
-        console.error(error);
-      });
-      return res.status(400).json({ error: 'Contract compilation failed' });
+  // Check if contract artifacts already exist
+  if (contractArtifacts[contractName]) {
+    // Use existing contract artifacts for deployment
+    try {
+      const { address } = contractArtifacts[contractName];
+      res.json({ address });
+    } catch (error) {
+      res.status(500).json({ error: 'Contract deployment failed' });
+    }
+  } else {
+    // Compile and deploy the contract
+    try {
+      const { address } = await compileAndDeployContract(contractName, templateName, name, symbol, decimals, totalSupply, privateKey);
+      res.json({ address });
+    } catch (error) {
+      res.status(500).json({ error: 'Contract deployment failed' });
     }
   }
+});
 
-  const contractBytecode = output.contracts[`${contractName}.sol`][`${contractName}`].evm.bytecode.object;
-  const contractAbi = JSON.parse(output.contracts[`${contractName}.sol`][`${contractName}`].metadata).output.abi;
+const compileAndDeployContract = (contractName, templateName, name, symbol, decimals, totalSupply, privateKey) => {
+  return new Promise(async (resolve, reject) => {
+    // Read the template contract
+    const contractTemplate = fs.readFileSync(path.join(__dirname, '..', 'Templates', `${templateName}.sol`), 'utf8');
 
-  const artifactsFolderPath = path.join(__dirname, '..', 'artifacts');
-  if (!fs.existsSync(artifactsFolderPath)) {
-    fs.mkdirSync(artifactsFolderPath);
-  }
+    // Replace the placeholder with the user-provided name in the contract template
+    const updatedContractSource = contractTemplate.replace('CONTRACTNAME', contractName);
 
-  const artifacts = {
-    contractName,
-    bytecode: contractBytecode,
-    abi: contractAbi,
-  };
-  // fs.writeFileSync(path.join(artifactsFolderPath, `${contractName}.json`), JSON.stringify(artifacts, null, 2));
-  artifactsCache[contractName] = artifacts;
-  console.log('\nContract compiled successfully.\n');
+    // Check if the replacement occurred successfully
+    if (updatedContractSource === contractTemplate) {
+      return reject({ error: "Placeholder 'CONTRACTNAME' not found in the contract template." });
+    }
 
-  const providerUrl = 'https://data-seed-prebsc-1-s1.binance.org:8545/';
-  const web3 = new Web3(providerUrl);
+    // Check if contract artifacts are already cached
+    if (contractArtifacts[contractName]) {
+      try {
+        const { address } = await deployContract(contractArtifacts[contractName].bytecode, contractArtifacts[contractName].abi, name, symbol, decimals, totalSupply, privateKey);
+        resolve({ address });
+      } catch (error) {
+        reject({ error: 'Contract deployment failed' });
+      }
+    } else {
+      // Compile the updated contract
+      const input = {
+        language: 'Solidity',
+        sources: {
+          [`${contractName}.sol`]: {
+            content: updatedContractSource,
+          },
+        },
+        settings: {
+          outputSelection: {
+            '*': {
+              '*': ['*'],
+            },
+          },
+          optimizer: {
+            enabled: true,
+            runs: 200, // Set the number of optimization runs here
+          },
+        },
+      };
 
-  const contract = new web3.eth.Contract(contractAbi);
-  web3.eth.accounts.wallet.add(privateKey);
-  const account = web3.eth.accounts.privateKeyToAccount(privateKey);
+      const output = JSON.parse(solc.compile(JSON.stringify(input)));
 
-  const deployOptions = {
-    data: '0x' + contractBytecode,
-    arguments: [name, symbol, decimals, totalSupply],
-  };
+      // Check for compilation errors
+      if (output.errors && output.errors.length > 0) {
+        const errors = output.errors.filter((error) => String(error).includes('Error'));
+        if (errors.length > 0) {
+          console.error('Contract compilation failed:');
+          errors.forEach((error) => {
+            console.error(error);
+          });
+          return reject({ error: 'Contract compilation failed' });
+        }
+      }
 
-  (async () => {
+      const contractBytecode = output.contracts[`${contractName}.sol`][`${contractName}`].evm.bytecode.object;
+      const contractAbi = JSON.parse(output.contracts[`${contractName}.sol`][`${contractName}`].metadata).output.abi;
+
+      try {
+        const address = await deployContract(contractBytecode, contractAbi, name, symbol, decimals, totalSupply, privateKey);
+        // Cache the contract artifacts for future use
+        contractArtifacts[contractName] = { bytecode: contractBytecode, abi: contractAbi, address };
+        resolve({ address });
+      } catch (error) {
+        reject({ error: 'Contract deployment failed' });
+      }
+    }
+  });
+};
+
+const deployContract = (bytecode, abi, name, symbol, decimals, totalSupply, privateKey) => {
+  return new Promise(async (resolve, reject) => {
+    const providerUrl = 'https://data-seed-prebsc-1-s1.binance.org:8545/';
+    const web3 = new Web3(providerUrl);
+
+    const contract = new web3.eth.Contract(abi);
+    web3.eth.accounts.wallet.add(privateKey);
+    const account = web3.eth.accounts.privateKeyToAccount(privateKey);
+
+    const deployOptions = {
+      data: '0x' + bytecode,
+      arguments: [name, symbol, decimals, totalSupply],
+    };
+
     try {
       const gasEstimate = await contract.deploy(deployOptions).estimateGas({ from: account.address });
       const gasPrice = await web3.eth.getGasPrice();
@@ -209,119 +234,16 @@ app.post('/api/deploy-bigeyes', async (req, res) => {
       });
 
       console.log("Before calling deploy function");
-      // let deployedContract = await deployment;
       const deployedContract = await deployment;
-      console.log("waiting to deploy");
-      const contractAddress = deployedContract.options.address;
-      console.log('Contract deployed at address:', contractAddress);
-      // res.json({ address: deployedContract.options.address });
+      console.log('Contract deployed at address:', deployedContract.options.address);
+      resolve({ address: deployedContract.options.address });
     } catch (error) {
       console.error(error);
-      res.status(500).json({ error: 'Contract deployment failed' });
+      reject('Contract deployment failed');
     }
-  })();
-});
+  });
+};
 
-app.post('/api/deploy-neocypherpunk', async (req, res) => {
-  const { contractName, templateName, router, name, symbol, privateKey } = req.body;
-
-  // Read the template contract
-  const contractTemplate = fs.readFileSync(path.join(__dirname, '..', 'templates', `${templateName}.sol`), 'utf8');
-
-  // Replace the placeholder with the user-provided name in the contract template
-  const updatedContractSource = contractTemplate.replace('CONTRACTNAME', contractName);
-
-  // Check if the replacement occurred successfully
-  if (updatedContractSource === contractTemplate) {
-    return res.status(400).json({ error: "Placeholder 'CONTRACTNAME' not found in the contract template." });
-  }
-
-  // Write the updated contract to a temporary file
-  fs.writeFileSync(path.join(__dirname, '..', 'generated', `${contractName}.sol`), updatedContractSource, 'utf8');
-
-  // Compile the updated contract
-  const contractSourceCode = fs.readFileSync(path.join(__dirname, '..', 'generated', `${contractName}.sol`), 'utf8');
-  const input = {
-    language: 'Solidity',
-    sources: {
-      [`${contractName}.sol`]: {
-        content: contractSourceCode,
-      },
-    },
-    settings: {
-      outputSelection: {
-        '*': {
-          '*': ['*'],
-        },
-      },
-      optimizer: {
-        enabled: true,
-        runs: 200, // Set the number of optimization runs here
-      },
-    },
-  };
-
-  const output = JSON.parse(solc.compile(JSON.stringify(input)));
-
-  // Check for compilation errors
-  if (output.errors && output.errors.length > 0) {
-    const errors = output.errors.filter((error) => String(error).includes('Error'));
-    if (errors.length > 0) {
-      console.error('Contract compilation failed:');
-      errors.forEach((error) => {
-        console.error(error);
-      });
-      return res.status(400).json({ error: 'Contract compilation failed' });
-    }
-  }
-
-  const contractBytecode = output.contracts[`${contractName}.sol`][`${contractName}`].evm.bytecode.object;
-  const contractAbi = JSON.parse(output.contracts[`${contractName}.sol`][`${contractName}`].metadata).output.abi;
-
-  const artifactsFolderPath = path.join(__dirname, '..', 'artifacts');
-  if (!fs.existsSync(artifactsFolderPath)) {
-    fs.mkdirSync(artifactsFolderPath);
-  }
-
-  const artifacts = {
-    contractName,
-    bytecode: contractBytecode,
-    abi: contractAbi,
-  };
-  fs.writeFileSync(path.join(artifactsFolderPath, `${contractName}.json`), JSON.stringify(artifacts, null, 2));
-  console.log('\nContract compiled successfully.\n');
-
-  const providerUrl = 'https://data-seed-prebsc-1-s1.binance.org:8545/';
-  const web3 = new Web3(providerUrl);
-
-  const contract = new web3.eth.Contract(contractAbi);
-  web3.eth.accounts.wallet.add(privateKey);
-  const account = web3.eth.accounts.privateKeyToAccount(privateKey);
-
-  const deployOptions = {
-    data: '0x' + contractBytecode,
-    arguments: [router, name, symbol],
-  };
-
-  try {
-    const gasEstimate = await contract.deploy(deployOptions).estimateGas({ from: account.address });
-    const gasPrice = await web3.eth.getGasPrice();
-    const deployment = contract.deploy(deployOptions).send({
-      from: account.address,
-      gas: gasEstimate,
-      gasPrice: gasPrice,
-    });
-
-    const deployedContract = await deployment;
-    console.log('Contract deployed at address:', deployedContract.options.address);
-    res.json({ address: deployedContract.options.address });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Contract deployment failed' });
-  }
-});
-
-const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
